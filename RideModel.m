c3f115156d2c91f24173b4c886fa1a28c8cc7a96
Elijah Perez <ElijahjacobPerez@gmail.com>
2024-09-27 17:42:44 -0700

    Clean up

--- RideModel.m
+++ RideModel.m
@@ -105,111 +105,122 @@ t_span = 0:sr:tmax;
 rideHeight = 0;
 
 %% Road input (janky) as a function meters + accleoration vehicle acceloration Input
+
 noiseLevel = 0;
 noise =rand(100000,1)*noiseLevel;
                          
 %Vehicle acceloration a(t)
-acceloration_Func= @(t) 10.^(-t);                                      %m/s^2                             
+    acceloration_Func_long= @(t) 10.^(-t);                                      %m/s^2 long                       
+    Vehic_acc_long = timeseries(acceloration_Func_long(t_span'),t_span);
+    Vehicle_acceloration_long = Vehic_acc_long.data;
 
-Vehic_acc = timeseries(acceloration_Func(t_span'),t_span);
-Vehicle_acceloration = Vehic_acc.data;
+    acceloration_Func_lat= @(t) 10.^(-t);                                      %m/s^2 lat                       
+    Vehic_acc_lat = timeseries(acceloration_Func_lat(t_span'),t_span);
+    Vehicle_acceloration_lat = Vehic_acc_lat.data;
+
 
 % Calc velocity and position
-x_i = 0;
-v_i = 0;    
+    x_i = 0;
+    v_i = 0;    
 
-for i = 1:length(t_span)
-Vehicle_Velocity(1,i) = v_i + Vehicle_acceloration(i,1).*t_span(1,i); % m/s 
-position(1,i) = x_i + v_i*t_span(1,i) + 0.5*Vehicle_acceloration(i,1).*t_span(1,i).^2;
-end
+    for i = 1:length(t_span)
+        Vehicle_Velocity(1,i) = v_i + Vehicle_acceloration_long(i,1).*t_span(1,i); % m/s 
+        position(1,i) = x_i + v_i*t_span(1,i) + 0.5*Vehicle_acceloration_long(i,1).*t_span(1,i).^2;
+    end
+
 
 % road inputs r(x)
-roadfl = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
-road_inputfl = timeseries(roadfl(position'),t_span);
+    roadfl = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
+    road_inputfl = timeseries(roadfl(position'),t_span);
 
-roadfr = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
-road_inputfr = timeseries(roadfr(position'),t_span);
+    roadfr = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
+    road_inputfr = timeseries(roadfr(position'),t_span);
 
-roadrr = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
-road_inputrr = timeseries(roadrr(position'),t_span);
+    roadrr = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
+    road_inputrr = timeseries(roadrr(position'),t_span);
 
-roadrl = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
-road_inputrl = timeseries(roadrl(position'),t_span);
+    roadrl = @(x) 0*sin(2*pi*(((r-f)/(2*T)*x.^2 ))) + noise(ceil(x*1+2000+1));
+    road_inputrl = timeseries(roadrl(position'),t_span);
 
 
-u_fl = road_inputfl;     %front left tire input
-u_fr = road_inputfr;     %front right tire input
-u_rl = road_inputrl;     %rear left tire input
-u_rr = road_inputrr;     %rear right tire input
-road_input_stored = zeros(length(u_fl.data),4);
-road_input_stored(:,1) = u_fr.data;
-road_input_stored(:,2) = u_fl.data;
-road_input_stored(:,3) = u_rr.data;
-road_input_stored(:,4) = u_rl.data;
-time = u_fr.Time;
+    u_fl = road_inputfl;     %front left tire input
+    u_fr = road_inputfr;     %front right tire input
+    u_rl = road_inputrl;     %rear left tire input
+    u_rr = road_inputrr;     %rear right tire input
+    road_input_stored = zeros(length(u_fl.data),4);
+    road_input_stored(:,1) = u_fr.data;
+    road_input_stored(:,2) = u_fl.data;
+    road_input_stored(:,3) = u_rr.data;
+    road_input_stored(:,4) = u_rl.data;
+    time = u_fr.Time;
 
 
 
 
 %% State space 
-IC = [0 0 0 0 0 0 0 0 0 0 0 0 0 0];
 
-A =[
- 0 1 0 0 0 0 0 0 0 0 0 0 0 0;
- (-kfl-kfr-krl-krr)/ms (-cfl-cfr-crl-crr)/ms (kfl)/ms (cfl)/ms (kfr)/ms (cfr)/ms (krl)/ms (crl)/ms (krr)/ms (crr)/ms (-kfl*lf-kfr*lf+krl*lr+krr*lr)/ms (-cfl*lf-cfr*lf+crl*lr+crr*lr)/ms (-kfl*laa+kfr*laa+krl*laa-krr*laa)/ms (-cfl*laa+cfr*laa+crl*laa-crr*laa)/ms;
- 0 0 0 1 0 0 0 0 0 0 0 0 0 0;
- (kfl)/mfl (cfl)/mfl (-kfl-kft)/mfl (-cfl)/mfl 0 0 0 0 0 0 (kfl*lf)/mfl (cfl*lf)/mfl (kfl*laa)/mfl (cfl*laa)/mfl;
- 0 0 0 0 0 1 0 0 0 0 0 0 0 0;
- (kfr)/mfr (cfr)/mfr 0 0 (-kfr-kft)/mfr (-cfr)/mfr 0 0 0 0 (kfr*lf)/mfr (cfr*lf)/mfr (kfr*laa)/mfr (cfr*laa)/mfr;
- 0 0 0 0 0 0 0 1 0 0 0 0 0 0;
- (krl)/mrl (crl)/mrl 0 0 0 0 (-krl-krt)/mrl (-crl)/mrl 0 0 (krl*lr)/mrl (crl*lr)/mrl (krl*laa)/mrl (crl*laa)/mrl;
- 0 0 0 0 0 0 0 0 0 1 0 0 0 0;
- (krr)/mrr (crr)/mrr 0 0 0 0 0 0 (-krr-krt)/mrr (-crr)/mrr (krr*lr)/mrr (crr*lr)/mrr (krr*laa)/mrr (crr*laa)/mrr;
- 0 0 0 0 0 0 0 0 0 0 0 1 0 0;
- (-kfl*lf-kfr*lr+krl*lf+krr*lr)/I_pitch (-cfl*lf-cfr*lr+crl*lf+crr*lr)/I_pitch -(kfl*lf)/I_pitch -(cfl*lf)/I_pitch -(kfr*lr)/I_pitch -(cfr*lr)/I_pitch (krl*lf)/I_pitch (crl*lf)/I_pitch (krr*lr)/I_pitch (crr*lr)/I_pitch (-kfl*lf^2-kfr*lr^2-krl*lf^2-krr*lr^2)/I_pitch (-cfl*lf^2-cfr*lr^2-crl*lf^2-crr*lr^2)/I_pitch 0 0;
- 0 0 0 0 0 0 0 0 0 0 0 0 0 1;
- (-kfl*laa-kfr*lbb+krl*laa+krr*lbb)/I_roll (-cfl*laa-cfr*lbb+crl*laa+crr*lbb)/I_roll -(kfl*laa)/I_roll -(cfl*laa)/I_roll (kfr*lbb)/I_roll (cfr*lbb)/I_roll -(krl*laa)/I_roll -(crl*laa)/I_roll (krr*lbb)/I_roll (crr*lbb)/I_roll 0 0 (-kfl*laa^2-kfr*lbb^2-krl*laa^2-krr*lbb^2)/I_roll (-cfl*laa^2-cfr*lbb^2-crl*laa^2-crr*lbb^2)/I_roll
-  ];
+% inital conditions
+    IC = [0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+    
 
-% B road inputs and external Moment_pitch
-
-B = [
-   0 0 0 0 0 0;
-   0 0 0 0 0 0;
-   0 0 0 0 0 0;
-   (kft/mfl) 0 0 0 0 0;  %front left tire
-   0 0 0 0 0 0;
-   0 (kft/mfr) 0 0 0 0;  %front right tire
-   0 0 0 0 0 0;
-   0  0 (krt/mfr) 0 0 0; %rear left tire
-   0 0 0 0 0 0;
-   0 0 0 (krt/mrr) 0 0;   %rear right tire
-   0 0 0 0 0 0;
-   0 0 0 0 1/I_pitch 0;
-   0 0 0 0 0 0;
-   0 0 0 0 0 1/I_roll];
+%State matrix    
+    A =[
+     0 1 0 0 0 0 0 0 0 0 0 0 0 0;
+     (-kfl-kfr-krl-krr)/ms (-cfl-cfr-crl-crr)/ms (kfl)/ms (cfl)/ms (kfr)/ms (cfr)/ms (krl)/ms (crl)/ms (krr)/ms (crr)/ms (-kfl*lf-kfr*lf+krl*lr+krr*lr)/ms (-cfl*lf-cfr*lf+crl*lr+crr*lr)/ms (-kfl*laa+kfr*laa+krl*laa-krr*laa)/ms (-cfl*laa+cfr*laa+crl*laa-crr*laa)/ms;
+     0 0 0 1 0 0 0 0 0 0 0 0 0 0;
+     (kfl)/mfl (cfl)/mfl (-kfl-kft)/mfl (-cfl)/mfl 0 0 0 0 0 0 (kfl*lf)/mfl (cfl*lf)/mfl (kfl*laa)/mfl (cfl*laa)/mfl;
+     0 0 0 0 0 1 0 0 0 0 0 0 0 0;
+     (kfr)/mfr (cfr)/mfr 0 0 (-kfr-kft)/mfr (-cfr)/mfr 0 0 0 0 (kfr*lf)/mfr (cfr*lf)/mfr (kfr*laa)/mfr (cfr*laa)/mfr;
+     0 0 0 0 0 0 0 1 0 0 0 0 0 0;
+     (krl)/mrl (crl)/mrl 0 0 0 0 (-krl-krt)/mrl (-crl)/mrl 0 0 (krl*lr)/mrl (crl*lr)/mrl (krl*laa)/mrl (crl*laa)/mrl;
+     0 0 0 0 0 0 0 0 0 1 0 0 0 0;
+     (krr)/mrr (crr)/mrr 0 0 0 0 0 0 (-krr-krt)/mrr (-crr)/mrr (krr*lr)/mrr (crr*lr)/mrr (krr*laa)/mrr (crr*laa)/mrr;
+     0 0 0 0 0 0 0 0 0 0 0 1 0 0;
+     (-kfl*lf-kfr*lr+krl*lf+krr*lr)/I_pitch (-cfl*lf-cfr*lr+crl*lf+crr*lr)/I_pitch -(kfl*lf)/I_pitch -(cfl*lf)/I_pitch -(kfr*lr)/I_pitch -(cfr*lr)/I_pitch (krl*lf)/I_pitch (crl*lf)/I_pitch (krr*lr)/I_pitch (crr*lr)/I_pitch (-kfl*lf^2-kfr*lr^2-krl*lf^2-krr*lr^2)/I_pitch (-cfl*lf^2-cfr*lr^2-crl*lf^2-crr*lr^2)/I_pitch 0 0;
+     0 0 0 0 0 0 0 0 0 0 0 0 0 1;
+     (-kfl*laa-kfr*lbb+krl*laa+krr*lbb)/I_roll (-cfl*laa-cfr*lbb+crl*laa+crr*lbb)/I_roll -(kfl*laa)/I_roll -(cfl*laa)/I_roll (kfr*lbb)/I_roll (cfr*lbb)/I_roll -(krl*laa)/I_roll -(crl*laa)/I_roll (krr*lbb)/I_roll (crr*lbb)/I_roll 0 0 (-kfl*laa^2-kfr*lbb^2-krl*laa^2-krr*lbb^2)/I_roll (-cfl*laa^2-cfr*lbb^2-crl*laa^2-crr*lbb^2)/I_roll
+      ];
 
 
-C = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 ; 
-      0 1 0 0 0 0 0 0 0 0 0 0 0 0 ;
-      0 0 1 0 0 0 0 0 0 0 0 0 0 0 ;
-      0 0 0 1 0 0 0 0 0 0 0 0 0 0 ;
-      0 0 0 0 1 0 0 0 0 0 0 0 0 0 ;
-      0 0 0 0 0 1 0 0 0 0 0 0 0 0 ;
-      0 0 0 0 0 0 1 0 0 0 0 0 0 0 ;
-      0 0 0 0 0 0 0 1 0 0 0 0 0 0 ;
-      0 0 0 0 0 0 0 0 1 0 0 0 0 0 ;
-      0 0 0 0 0 0 0 0 0 1 0 0 0 0 ;
-      0 0 0 0 0 0 0 0 0 0 1 0 0 0 ;
-      0 0 0 0 0 0 0 0 0 0 0 1 0 0 ;
-      0 0 0 0 0 0 0 0 0 0 0 0 1 0 ;
-      0 0 0 0 0 0 0 0 0 0 0 0 0 1 ;
-      (-kfl-kfr-krl-krr)/ms (-cfl-cfr-crl-crr)/ms (kfl)/ms (cfl)/ms (kfr)/ms (cfr)/ms (krl)/ms (crl)/ms (krr)/ms (crr)/ms (-kfl*lf-kfr*lf+krl*lr+krr*lr)/ms (-cfl*lf-cfr*lf+crl*lr+crr*lr)/ms (-kfl*laa+kfr*laa+krl*laa-krr*laa)/ms (-cfl*laa+cfr*laa+crl*laa-crr*laa)/ms];
+% Road inputs and external forces
+    B = [
+       0 0 0 0 0 0;
+       0 0 0 0 0 0;
+       0 0 0 0 0 0;
+       (kft/mfl) 0 0 0 0 0;   %front left tire
+       0 0 0 0 0 0;
+       0 (kft/mfr) 0 0 0 0;   %front right tire
+       0 0 0 0 0 0;
+       0  0 (krt/mfr) 0 0 0;  %rear left tire
+       0 0 0 0 0 0;
+       0 0 0 (krt/mrr) 0 0;   %rear right tire
+       0 0 0 0 0 0;
+       0 0 0 0 1/I_pitch 0;   %Pitch
+       0 0 0 0 0 0;
+       0 0 0 0 0 1/I_roll];   %Roll
+
+
+% Outputs 
+    C = [1 0 0 0 0 0 0 0 0 0 0 0 0 0 ; 
+          0 1 0 0 0 0 0 0 0 0 0 0 0 0 ;
+          0 0 1 0 0 0 0 0 0 0 0 0 0 0 ;
+          0 0 0 1 0 0 0 0 0 0 0 0 0 0 ;
+          0 0 0 0 1 0 0 0 0 0 0 0 0 0 ;
+          0 0 0 0 0 1 0 0 0 0 0 0 0 0 ;
+          0 0 0 0 0 0 1 0 0 0 0 0 0 0 ;
+          0 0 0 0 0 0 0 1 0 0 0 0 0 0 ;
+          0 0 0 0 0 0 0 0 1 0 0 0 0 0 ;
+          0 0 0 0 0 0 0 0 0 1 0 0 0 0 ;
+          0 0 0 0 0 0 0 0 0 0 1 0 0 0 ;
+          0 0 0 0 0 0 0 0 0 0 0 1 0 0 ;
+          0 0 0 0 0 0 0 0 0 0 0 0 1 0 ;
+          0 0 0 0 0 0 0 0 0 0 0 0 0 1 ;
+          (-kfl-kfr-krl-krr)/ms (-cfl-cfr-crl-crr)/ms (kfl)/ms (cfl)/ms (kfr)/ms (cfr)/ms (krl)/ms (crl)/ms (krr)/ms (crr)/ms (-kfl*lf-kfr*lf+krl*lr+krr*lr)/ms (-cfl*lf-cfr*lf+crl*lr+crr*lr)/ms (-kfl*laa+kfr*laa+krl*laa-krr*laa)/ms (-cfl*laa+cfr*laa+crl*laa-crr*laa)/ms];
 
 
 
-D = zeros(15,6);
-D(15,5) = 1/ms;
+    D = zeros(15,6);
+    D(15,5) = 1/ms;
 %% running simulink, extracting from simulink
 
 
